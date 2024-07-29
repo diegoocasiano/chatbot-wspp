8e9a9b7392c27345486072a4480d2bd031c0b81f
Diego Casiano <dianlonso02@gmail.com>
2024-07-29 11:45:36 -0500

    change5

--- dowldr-mp3.py
+++ dowldr-mp3.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from pytube import YouTube
 import ffmpeg
+import os
 
 app = Flask(__name__)
 
@@ -10,42 +11,32 @@ def download():
         # URL del video a descargar
         yt_link = request.json['link']
 
-        # Obtengo el titulo del video con yt_dlp
-        def get_video_title(yt_link):
-            yt = YouTube(yt_link)
-            return yt.title
-        
-
-        video_title = get_video_title(yt_link)
+        # Obtengo el título del video con pytube
+        yt = YouTube(yt_link)
+        video_title = yt.title
         print(f"Descargando audio de: {video_title}")
         
         # Descargar el audio desde YouTube con pytube
-        yt = YouTube(yt_link)
-
-        # Ordenamos los streams (o flujos) de mayor a menor según su bitrate
         audio_streams = yt.streams.filter(only_audio=True).order_by('abr').desc()
-
-        # Seleccionamos el stream (o flujo) con mayor bitrate
         top_abr_stream = audio_streams.first()
 
-        # Descargamos el stream (o flujo) seleccionado y lo guardamos como audio.webm
+        # Descargamos el stream seleccionado y lo guardamos como audio.webm
         top_abr_stream.download(filename='audio.webm')
 
-        # Cargamos el archivo, descargado por pytube, con ffmpeg
+        # Convertimos el archivo webm a mp3 con ffmpeg
         input_file = ffmpeg.input('audio.webm')
-
-        # Convertimos el archivo webm a mp3
         output_file = input_file.output(f'{video_title}.mp3')
-        
-        # Inicia la conversión con ffmpeg
         output_file.run(overwrite_output=True)
+
+        # Eliminar el archivo temporal .webm
+        os.remove('audio.webm')
 
         print(f"Descarga exitosa: {video_title}.mp3")
         return jsonify({"message": "Descarga exitosa", "file_name": f'{video_title}.mp3'}), 200
 
     except Exception as e:
         print(f"Error al descargar el audio: {str(e)}")
-        return jsonify({"error": "Error al descargar el audio"}), 500
+        return jsonify({"error": f"Error al descargar el audio: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5001)
